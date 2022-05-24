6ea0dfc2b6176181d8b7407e9d27d165487689bf
colaborador <colaborador2@adeadigital.com>
2022-05-24 14:55:01 +0200

    funcion que analiza duplicados

--- pruebas.js
+++ pruebas.js
@@ -8,33 +8,42 @@ var sliced3 = []
 
 var maxLines = 3 // Lineas maximas de pagarés por documento
 var slicedDoc = [];
-var count;
+var count = 0
+var isDuplicated
+var duplicatedPosition = 0
 
 for (var i = 0; i < arr1.length; i += maxLines) {
-    if(arr1[i]==arr1[i+1]){
-        sameID++
-    }
-    if (isDuplicated == false) {
-        sliced1.push([arr1.slice(i, i + maxLines)]);
-        sliced2.push([arr2.slice(i, i + maxLines)]);
-        sliced3.push([arr3.slice(i, i + maxLines)]);
-    }
+    sliced1.push([arr1.slice(i, i + maxLines)]);
+    sliced2.push([arr2.slice(i, i + maxLines)]);
+    sliced3.push([arr3.slice(i, i + maxLines)]);
 }
-
-
 console.table(sliced1);
 console.table(sliced2);
 console.table(sliced3);
 
-var isDuplicated
 
 for (var i = 0; i < sliced1.length; i++) {
-    sliced1[i + 1].forEach(y => {
-        isDuplicated = (sliced1[i].indexOf(y) != -1);
-    });
-    console.log("*" + isDuplicated)
-
+    console.log("Documento " + (i + 1) + ":")
+    console.table(sliced1[i])
+    for (var j = 0; j < maxLines; j++) {
+        if (sliced1[j] == sliced1[j+1]) {
+            console.log("Repetido")
+        }
+        else{
+            console.log("Hay un valor distinto")
+        }
+    }
 }
+
+
+
+// for (var i = 0; i < sliced1.length; i++) {
+//     sliced1[i + 1].forEach(y => {
+//         isDuplicated = (sliced1[i].indexOf(y) != -1);
+//     });
+//     console.log("*" + isDuplicated)
+
+// }
 
 
 
